13e4da9467123b8ad9a618387b2de6c51bedd5c6
PaulCharbonneau <paul.charbonneau@polymtl.ca>
2024-10-02 13:11:15 -0400

    Add file: setup.py to create a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='selection_quartiers',
+      version='0.1',
+      description='Utilitaire pour choisir les quartiers dans une ville',
+      url='https://github.com/EPMPaulPoly/selection_quartier',
+      author='Paul Charbonneau',
+      author_email='paulcharbon@gmail.com',
+      license='MIT',
+      packages=['selection_quartiers'],
+      # zip_safe=False
+)
